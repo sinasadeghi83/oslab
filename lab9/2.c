6ea40208f0e8ccea8d37d40f1569db616b6a4d57
Sina Sadeghi <sina.sadeghi83@gmail.com>
2024-12-25 19:36:13 +0330

    Fix lab9.2

--- lab9/2.c
+++ lab9/2.c
@@ -5,72 +5,80 @@
 
 #define MAX_COUNT 10
 
-int buffer = 0;            
-int reader_count = 0;       
-sem_t resource_access;      
-sem_t reader_count_access; 
+int buffer = 0;
+int reader_count = 0;
+sem_t resource_access;
+sem_t wait_reader;
+sem_t wait_writer;
 int stop = 0;
-int stop_reader[2];
-int isread[2];               
 
-void *reader(void *arg) {
+void *reader(void *arg)
+{
     long id = (long)arg;
-    while (1) {
-
-        if (stop_reader[id]) {
-            sem_post(&reader_count_access);
+    int lastread = -1;
+    while (1)
+    {
+        sem_wait(&wait_writer);
+        if (stop)
+        {
+            sem_post(&wait_writer);
             break;
         }
-        
-        if (reader_count == 1) {
-            sem_wait(&resource_access); 
-        }
-
-        printf("Reader %ld reads buffer value: %d\n", id, buffer);
-        isread[id] = 1;
-        if(stop){
-            stop_reader[id] = 1;
-        }
-        sleep(1);
-
-
-        if (isread[0] && isread[1]) {
-            isread[0] = isread[1] = 0;
+        sem_wait(&resource_access);
+        if (lastread == buffer)
+        {
+            sem_post(&wait_writer);
             sem_post(&resource_access);
+            continue;
         }
-
+        printf("Reader %ld reads buffer value: %d\n", id, buffer);
+        lastread = buffer;
+        // sleep(1);
+        sem_post(&wait_reader);
+        sem_post(&resource_access);
     }
 
     printf("Reader %ld stopped.\n", id);
     return NULL;
 }
 
-void *writer(void *arg) {
+void *writer(void *arg)
+{
     long id = (long)arg;
-    while (1) {
+    while (1)
+    {
         sem_wait(&resource_access);
-        if (buffer >= MAX_COUNT) {
-            stop = 1;
-            sem_post(&resource_access);
-            break;
-        }
-
         buffer++;
         printf("Writer %ld writes buffer value: %d\n", id, buffer);
-        sleep(1);
-
+        // sleep(1);
         sem_post(&resource_access);
+        sem_post(&wait_writer);
+        sem_wait(&wait_reader);
+        sem_post(&wait_writer);
+        sem_wait(&wait_reader);
+        if (buffer == MAX_COUNT)
+        {
+            sem_post(&wait_writer);
+            stop = 1;
+            break;
+        }
     }
 
     printf("Writer %ld stopped.\n", id);
     return NULL;
 }
 
-int main() {
+int main()
+{
     pthread_t readers[2], writer_thread;
 
     sem_init(&resource_access, 0, 1);
-    sem_init(&reader_count_access, 0, 1);
+    sem_init(&wait_reader, 0, 2);
+    sem_init(&wait_writer, 0, 1);
+
+    sem_wait(&wait_reader);
+    sem_wait(&wait_reader);
+    sem_wait(&wait_writer);
 
     pthread_create(&writer_thread, NULL, writer, (void *)1);
     pthread_create(&readers[0], NULL, reader, (void *)1);
@@ -81,7 +89,8 @@ int main() {
 
     // Clean up semaphores
     sem_destroy(&resource_access);
-    sem_destroy(&reader_count_access);
+    sem_destroy(&wait_writer);
+    sem_destroy(&wait_reader);
 
     return 0;
 }
